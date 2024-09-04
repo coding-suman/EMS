5ffe17ea1851644e30a2c42dc7eea971481c25a0
SUMAN MANDAL <iamsumandal@gmail.com>
2024-09-04 17:43:50 +0530

    done

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
-import { toast } from 'react-toastify';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api', // Set your API base URL
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
 });
 
-// Interceptor for handling responses
 instance.interceptors.response.use(
   (response) => response,
   (error) => {
     const message = error.response?.data?.message || "An unexpected error occurred";
-    // toast.error(message);
     return Promise.reject(error);
   }
 );
